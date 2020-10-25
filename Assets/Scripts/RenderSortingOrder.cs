d78356d77412539caa99297fb0bb0dc67023b418
John <douglas.sousa@gmail.com>
2020-10-25 17:36:36 +0900

    Render order and population of the rest of the map

---
+++ Assets/Scripts/RenderSortingOrder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[ExecuteAlways]
+public class RenderSortingOrder : MonoBehaviour
+{
+    private Renderer _renderer;
+    [SerializeField] private int baseOrder = 5000;
+    [SerializeField] private int offset = 0;
+    [SerializeField] private bool runOnce = false;
+
+    private void Awake()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+    }
+    
+    public void FixedUpdate()
+    {
+        _renderer.sortingOrder = (int) (baseOrder - transform.position.y - offset);
+        if(runOnce)
+            Destroy(this);
+    }
+}
